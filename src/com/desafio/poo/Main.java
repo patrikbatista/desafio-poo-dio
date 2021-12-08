ef7b6f5d9e9e3b30a9167362f1b11fab23050f33
patrik <pcr_batista@hotmail.com>
2021-12-08 13:08:34 -0300

    Implementa as classes Curso e Mentoria

--- src/com/desafio/poo/Main.java
+++ src/com/desafio/poo/Main.java
@@ -1,8 +1,21 @@
 package com.desafio.poo;
 
+import com.desafio.poo.dominio.Curso;
+import com.desafio.poo.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Curso curso1 = new Curso("Curso Java", "descrição curso Java", 8); //instancia um novo curso através do Constructor;
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso("Curso Js", "Curso de JavaScript", 4);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria("Mentoria de Java", "bootcamp Spread", LocalDate.now());
+        System.out.println(mentoria);
     }
 }
